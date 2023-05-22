619556ce8d3b72ca11285a947f9123480c6d4423
DanielMarella <marella.daniel96@gmail.com>
2023-05-22 16:33:56 +0200

    create first three request of the exercise, i will create the function later, just done this for try if it's work

--- pari e dispari/js/myscript.js
+++ pari e dispari/js/myscript.js
@@ -9,8 +9,16 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
-Consigli del giorno
-Scriviamo sempre in italiano i passaggi che vogliamo fare
-Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
-
 */
+
+
+// Chiedo all'utente di scegliere pari  o dispari
+const userChoise = prompt('Pari o dispari?')
+// chiedo all'utente di scrivere un numero da 1 a 5
+const userNumber = prompt ('inserisci un numero da 1 a 5');
+
+// creiamo un numero randomico da 1 a 5 per il computer
+
+const computerNumber = Math.floor((Math.random() * 5) + 1);
+console.log(computerNumber);
+
