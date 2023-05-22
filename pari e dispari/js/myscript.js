194d329c9bbd4886bcc19d481dcc9d2cc4689590
DanielMarella <marella.daniel96@gmail.com>
2023-05-22 16:46:41 +0200

    create the sum of the number choise by computer and user

--- pari e dispari/js/myscript.js
+++ pari e dispari/js/myscript.js
@@ -14,10 +14,16 @@ Dichiariamo chi ha vinto.
 
 // Chiedo all'utente di scegliere pari  o dispari
 const userChoise = prompt('Pari o dispari?')
+console.log("l'utente ha scelto: " + userChoise)
+
 // chiedo all'utente di scrivere un numero da 1 a 5
-const userNumber = prompt ('inserisci un numero da 1 a 5');
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
+console.log("l'utente ha scelto il numero " + userNumber);
 
 // creiamo un numero randomico da 1 a 5 per il computer
 const computerNumber = Math.floor((Math.random() * 5) + 1);
-console.log(computerNumber);
+console.log('il computer ha scelto il numero: ' + computerNumber);
 
+// creiamo una variabile dove vengono sommati i due numeri scelti dal computer e dall'utente
+const sumNumber = userNumber + computerNumber;
+console.log(sumNumber);
