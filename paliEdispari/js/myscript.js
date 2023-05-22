84519b4642c1bf2c004cf2be8c7432768b4a248f
DanielMarella <marella.daniel96@gmail.com>
2023-05-22 15:47:49 +0200

    create first cyclo for analize the word letter by letter

--- paliEdispari/js/myscript.js
+++ paliEdispari/js/myscript.js
@@ -17,3 +17,21 @@ Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andia
 
 
 
+
+// Chiediamo all'utente di inserire una parola con il prompt
+const userWord = prompt('inserisci una parola');
+
+// Una parola è palindroma se i caratteri più a sx e più a dx coincidono e se la parte tra loro compresa è palindroma.
+// Devo controllare se i caratteri a sx e destra coincidono, quindi creo una variabile che mi dica quanto è lunga la parola
+const wordFootage = userWord.length;
+
+
+// creo un ciclo che mi controlli lettera per lettera se sono uguali o no
+for (let i = 0; i <= wordFootage; i++){
+    if(userWord[i] !== userWord[wordFootage]){
+
+        console.log(userWord[i])
+    }
+
+}
+
